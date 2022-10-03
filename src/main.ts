cb510248673aa3ad3de80c8d3a229d20bdf0ca61
hunghg255 <hunghg.agiletech@gmail.com>
2022-10-04 04:55:56 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
-  app.enableCors();
-  
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   const config = new DocumentBuilder()
     .setTitle('Agiletech test')
     .setVersion('1.0')
