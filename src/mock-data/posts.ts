259b56976eb811202633fc66f39976fbd4fca759
hunghg255 <hunghg.agiletech@gmail.com>
2022-10-04 04:59:09 +0700

    update

--- src/mock-data/posts.ts
+++ src/mock-data/posts.ts
@@ -1,100 +1,34 @@
 import { tags } from './tags';
 import { faker } from '@faker-js/faker';
 
+const genPost = () => {
+  return {
+    posts: new Array(30).fill(null).map((it) => ({
+      id: faker.datatype.uuid(),
+      title: faker.word.adjective(),
+      description: faker.lorem.paragraph(),
+      tags: [
+        faker.helpers.arrayElement(tags),
+        faker.helpers.arrayElement(tags),
+      ],
+    })),
+    current_page: 1,
+    total_page: 3,
+    page_size: 10,
+    total: 30,
+  };
+};
+
 export const postDummy = {
-  admin: {
-    posts: new Array(30).fill(null).map((it) => ({
-      id: faker.datatype.uuid(),
-      title: faker.word.adjective(),
-      description: faker.lorem.paragraph(),
-      tags: [
-        faker.helpers.arrayElement(tags),
-        faker.helpers.arrayElement(tags),
-      ],
-    })),
-    current_page: 1,
-    total_page: 3,
-    page_size: 10,
-    total: 30,
-  },
+  admin: genPost(),
 
-  admin1: {
-    posts: new Array(30).fill(null).map((it) => ({
-      id: faker.datatype.uuid(),
-      title: faker.word.adjective(),
-      description: faker.lorem.paragraph(),
-      tags: [
-        faker.helpers.arrayElement(tags),
-        faker.helpers.arrayElement(tags),
-      ],
-    })),
-    current_page: 1,
-    total_page: 3,
-    page_size: 10,
-    total: 30,
-  },
+  admin1: genPost(),
 
-  admin2: {
-    posts: new Array(30).fill(null).map((it) => ({
-      id: faker.datatype.uuid(),
-      title: faker.word.adjective(),
-      description: faker.lorem.paragraph(),
-      tags: [
-        faker.helpers.arrayElement(tags),
-        faker.helpers.arrayElement(tags),
-      ],
-    })),
-    current_page: 1,
-    total_page: 3,
-    page_size: 10,
-    total: 30,
-  },
+  admin2: genPost(),
 
-  adminRefresh: {
-    posts: new Array(30).fill(null).map((it) => ({
-      id: faker.datatype.uuid(),
-      title: faker.word.adjective(),
-      description: faker.lorem.paragraph(),
-      tags: [
-        faker.helpers.arrayElement(tags),
-        faker.helpers.arrayElement(tags),
-      ],
-    })),
-    current_page: 1,
-    total_page: 3,
-    page_size: 10,
-    total: 30,
-  },
+  adminRefresh: genPost(),
 
-  adminRefresh1: {
-    posts: new Array(30).fill(null).map((it) => ({
-      id: faker.datatype.uuid(),
-      title: faker.word.adjective(),
-      description: faker.lorem.paragraph(),
-      tags: [
-        faker.helpers.arrayElement(tags),
-        faker.helpers.arrayElement(tags),
-      ],
-    })),
-    current_page: 1,
-    total_page: 3,
-    page_size: 10,
-    total: 30,
-  },
+  adminRefresh1: genPost(),
 
-  adminRefresh2: {
-    posts: new Array(30).fill(null).map((it) => ({
-      id: faker.datatype.uuid(),
-      title: faker.word.adjective(),
-      description: faker.lorem.paragraph(),
-      tags: [
-        faker.helpers.arrayElement(tags),
-        faker.helpers.arrayElement(tags),
-      ],
-    })),
-    current_page: 1,
-    total_page: 3,
-    page_size: 10,
-    total: 30,
-  },
+  adminRefresh2: genPost(),
 };
