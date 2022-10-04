8f266b19dd0079e04a3bf3db838f2a55ac4d34c2
hunghg255 <hunghg.agiletech@gmail.com>
2022-10-04 10:08:11 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
   });
 
   const config = new DocumentBuilder()
