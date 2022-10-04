39d8ceff3d9b9186b97ba963113c92440d913a62
hunghg255 <hunghg.agiletech@gmail.com>
2022-10-04 10:39:03 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
   app.enableCors();
 
   const config = new DocumentBuilder()
@@ -22,5 +26,6 @@ async function bootstrap() {
   });
 
   await app.listen(3000);
+  console.log(`Server started running on http://localhost:3000`, 'Bootstrap');
 }
 bootstrap();
